62abfa26a18559afc62fb0193fdb71d14d7dbfb5
LsxnH <liheng86@mail.ustc.edu.cn>
2018-03-12 16:28:13 +0100

    gitlab operations

---
+++ git.sh
@@ -0,0 +1,24 @@
+git pull
+git add accessROOTfile.py
+git commit macros/plotL1Trigger.py -m "adding a macro for accessing a ROOT file"
+git push -u origin master #or just: git push
+cd $TestArea/PhysicsRPC/PhysicsRPCPlot
+git status
+git pull
+
+
+git pull
+modify the files
+git checkout AnpBatch_heng 
+git comment
+git push origin AnpBatch_heng
+submit a merge request 
+merged
+git checkout master #will find the modified files recover 
+git pull # get the changes
+
+
+git tag -a v18.03.12 -m 'version 18.03.12'
+git push --tags
+
+
